2fa428ee156483ec0ed569a4f10a33dd0d6ecb66
davi <manelv8@gmail.com>
2021-08-29 15:55:52 -0300

    project section

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,23 @@
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `
 
+var slider = tns({
+  container: '.my-slider',
+  items: 1,
+  nav:false,
+  controls: false,
+  mouseDrag: true,
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 2
+    },
+    700: {
+      gutter: 30
+    },
+    900: {
+      items: 3
+    }
+  }
+});
